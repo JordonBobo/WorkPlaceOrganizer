210913dc5b7fa87958b0e9f34f78ae22ffcba415
Jordon Bobo <jordon.bobo@outlook.com>
2021-03-04 15:58:20 -0700

    initial post, commented out specs for project.

---
+++ index.js
@@ -0,0 +1,52 @@
+
+
+// Takes user input and makes an HTML page
+
+// emails link to local email pre fills out an email (copy from profile, week8)
+// github info links to a new tab
+
+//      Info that it takes:
+// -team manager’s name
+// -employee ID 
+// -email address
+// -office number
+// - option of either adding an intern or engineer, or to complete the team
+// -- if choose engineer or intern, it then promps the above questions for that person.
+
+// ** This is a TDD - Test drivien dev:
+
+// __tests__/			// jest tests
+//   Employee.test.js
+//   Engineer.test.js
+//   Intern.test.js
+//   Manager.test.js
+// dist/               // rendered output (HTML) and CSS style sheet
+// lib/				// classes
+// src/				// template helper code
+// index.js			// runs the application
+
+
+
+// must have 4 employee types, manager, engineer, intern, and employee 
+// This gets tested with "__tests__/"
+
+// Lot of other test requirments, ugh. Look at them in the original readme
+
+
+// Good idea to have validation in the user input to ensure they can't put bad info
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
